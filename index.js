428d433483da8b43d344afc4e4b86e5ca390adcb
Xenia <kakirilyuk@gmail.com>
2022-04-13 10:14:57 -0400

    commit tree 6ab6291f590af768ec4d22ec4e288686450a09bd

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+const element = document.querySelector('main');
+element.remove();
+
+// const newHeader = document.createElement("h1");
+//document.body.append(newHeader);
+//const victory1 = document.createTextNode("victory");
+//newHeader.appendChild(victory1)
+
+//const champion = document.createTextNode("YOUR-NAME is the champion");
+
+//victory.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
+
+//const victory = document.getElementById("victory");
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
